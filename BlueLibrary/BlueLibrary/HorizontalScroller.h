2aa6ecfd3dc2f86a498609db4150f2cdc4139438
Olga Zilber <olga.mirensky@gmail.com>
2014-07-20 11:24:53 +1000

    Adapter pattern. Operational HorizontalScroller. (the album covers are not yet downloaded - next step)

---
+++ BlueLibrary/BlueLibrary/HorizontalScroller.h
@@ -0,0 +1,13 @@
+//
+//  HorizontalScroller.h
+//  BlueLibrary
+//
+//  Created by Olga on 18/07/14.
+//  Copyright (c) 2014 Eli Ganem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HorizontalScroller : UIView
+
+@end
---
+++ BlueLibrary/BlueLibrary/HorizontalScroller.m
@@ -0,0 +1,31 @@
+//
+//  HorizontalScroller.m
+//  BlueLibrary
+//
+//  Created by Olga on 18/07/14.
+//  Copyright (c) 2014 Eli Ganem. All rights reserved.
+//
+
+#import "HorizontalScroller.h"
+
+@implementation HorizontalScroller
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
